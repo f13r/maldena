2005a6cb1bf3dce8cb42198a86dfa8af8658ebe6
Victor Slavinskiy <victor@clioonline.dk>
2015-11-11 22:04:27 +0200

    dev doctrine cli fixed

--- bin/doctrine.php
+++ bin/doctrine.php
@@ -21,53 +21,46 @@ use Symfony\Component\Console\Helper\HelperSet;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 $autoloadFiles = array(__DIR__ . '/../vendor/autoload.php',
-                       __DIR__ . '/../../../autoload.php');
+	__DIR__ . '/../../../autoload.php');
 
 foreach ($autoloadFiles as $autoloadFile) {
-    if (file_exists($autoloadFile)) {
-        require_once $autoloadFile;
-    }
+	if (file_exists($autoloadFile)) {
+		require_once $autoloadFile;
+	}
 }
 
 $directories = array(getcwd(), getcwd() . DIRECTORY_SEPARATOR . 'config');
 
 $configFile = null;
 foreach ($directories as $directory) {
-    $configFile = $directory . DIRECTORY_SEPARATOR . 'cli-config.php';
+	$configFile = $directory . DIRECTORY_SEPARATOR . 'cli-config.php';
 
-    if (file_exists($configFile)) {
-        break;
-    }
+	if (file_exists($configFile)) {
+		break;
+	}
 }
 
 if ( ! file_exists($configFile)) {
-    ConsoleRunner::printCliConfigTemplate();
-    exit(1);
+	ConsoleRunner::printCliConfigTemplate();
+	exit(1);
 }
 
 if ( ! is_readable($configFile)) {
-    echo 'Configuration file [' . $configFile . '] does not have read permission.' . "\n";
-    exit(1);
+	echo 'Configuration file [' . $configFile . '] does not have read permission.' . "\n";
+	exit(1);
 }
 
-$commands = array(
- new \Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand(),
-    new \Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand(),
-    new \Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand(),
-    new \Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand(),
-    new \Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand(),
-    new \Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand()
-    );
+$commands = array();
 
 $helperSet = require $configFile;
 
 if ( ! ($helperSet instanceof HelperSet)) {
-    foreach ($GLOBALS as $helperSetCandidate) {
-        if ($helperSetCandidate instanceof HelperSet) {
-            $helperSet = $helperSetCandidate;
-            break;
-        }
-    }
+	foreach ($GLOBALS as $helperSetCandidate) {
+		if ($helperSetCandidate instanceof HelperSet) {
+			$helperSet = $helperSetCandidate;
+			break;
+		}
+	}
 }
 
 \Doctrine\ORM\Tools\Console\ConsoleRunner::run($helperSet, $commands);
